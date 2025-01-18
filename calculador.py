3475c7841773dcd62123b2e533ea2aaea816e2ac
Kottasx <kottszzerlast@gmail.com>
2025-01-18 16:28:32 -0300

    Calculador.py

---
+++ calculador.py
@@ -0,0 +1,42 @@
+# Mensagem de boas vindas:
+print('Seja bem-vindo(a) a calculadora!')
+
+# Tabela de Valores:
+num1 = float(input('Digite o primeiro valor'))
+num2 = float(input('Digite o segundo valor'))
+
+# Mensagens de sobre metodos de calculo:
+print('selecione o metodo de calculo.')
+print('Soma (+)')
+print('Subtração (-)')
+print('Divisão (:)')
+print('Multiplicação (x)')
+
+# Funções:
+operação = input('digite o numero desejado.')
+
+if operação == '1'
+resultado = num1 + num2
+print('[resultado]: {num1} + {num2} = {resultado}')
+
+elif operação == '2'
+resultado = num1 - num2
+print('[resultado]: {num1} - {num2} = {resultado}')
+
+elif operação == '3'
+resultado = num1 * num2
+print('[resultado]: {num1} * {num2} = {resultado}')
+
+elif operação == '4':
+if num1 !=0:
+ resultado (float(input)) = num1 / num2
+else:
+print('Resultado final: {num1} / {num2} = {resultado}')
+else:
+print('Divisão por zero é não é possivel!')
+else:
+print('Operação invalida!')
+
+
+# Adeus:
+print('Obrigado por utilizar a calculadora!.')
